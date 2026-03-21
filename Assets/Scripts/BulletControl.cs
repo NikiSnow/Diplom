3784289ffa9eb7b4e2811907e8da50fbeb286867
paukuss <88683452+PAUKUSS@users.noreply.github.com>
2026-03-21 18:13:04 +0300

    zz

--- Assets/Scripts/BulletControl.cs
+++ Assets/Scripts/BulletControl.cs
@@ -2,8 +2,55 @@ using UnityEngine;
 
 public class BulletControl : MonoBehaviour
 {
+    [SerializeField] private float freezeDuration = 2.5f;
+    [SerializeField] private float lifeTime = 5f;
+
+    private bool hasImpacted;
+
+    private void Awake()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(this.gameObject);
+        HandleImpact(collision.collider);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        HandleImpact(other);
+    }
+
+    private void HandleImpact(Collider2D other)
+    {
+        if (hasImpacted || other == null)
+            return;
+
+        if (IsPlayerCollider(other))
+            return;
+
+        hasImpacted = true;
+
+        FishController fish = other.GetComponent<FishController>();
+        if (fish == null)
+            fish = other.GetComponentInParent<FishController>();
+
+        if (fish != null)
+            fish.Freeze(freezeDuration);
+
+        Destroy(gameObject);
+    }
+
+    private bool IsPlayerCollider(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            return true;
+
+        if (other.attachedRigidbody != null && other.attachedRigidbody.gameObject.CompareTag("Player"))
+            return true;
+
+        Transform root = other.transform.root;
+        return root != null && root.CompareTag("Player");
     }
 }
